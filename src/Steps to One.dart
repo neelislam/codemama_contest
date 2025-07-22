1d336b97ac0a603106dc78af6174f5941c8325bd
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-07-22 11:22:35 +0600

    Knapsack 1

--- src/Steps to One.dart
+++ src/Steps to One.dart
@@ -24,3 +24,28 @@ void main() {
   }
   print(dp[N]);
 }
+
+/*Steps to One
+
+EN
+বাং
+Problem Statement
+Given a positive integer n, write a program that returns the minimum number of steps to reach 1 by performing one of the following operations: Subtract 1 from it. If it is divisible by 2, divide it by 2. If it is divisible by 3, divide it by 3. For example- from 5 , subtracting 1 it will be 4, then it will be divided by 2 and become 2 then again it will be divided by 2 and become 1. Total 3 steps will be needed.
+
+Input
+The program will take an integer
+N
+N as input.
+
+Output
+The output will print the number of steps to become 1.
+
+Constraints
+0 ≤ |N| ≤ 1000
+Example:
+Enter number
+
+Input:
+5
+Output:
+3*/
